37e01bf74e9c09f4ca1b0906094e953704b60faa
IT12666 <Anson.12666@gmail.com>
2021-06-26 15:47:35 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -5,7 +5,7 @@ setup=$currdir/_ESSENTIAL/Setup.txt
 if [ "$(grep -F "Sys.updated=" $setup | cut -d "=" -f2)" == "1" ]; then echo "Latest Version"
 else curl -L -o $currdir/update.sh https://github.com/IT12666/subs_translate/releases/download/0.0.0/update.sh $currdir 
 chmod +x $currdir/update.sh && exec $currdir/update.sh && echo "Updating" && exec $currdir/update.sh 
-rm -f $currdir/update.sh
+echo rm -f $currdir/update.sh
 fi 
 
 dep_check() { if ! command -v $1 > /dev/null; then echo $1 is missing
