c67403a3bd0d386baf35aa236169a8d74ed04e88
IT12666 <Anson.12666@gmail.com>
2021-06-27 15:53:58 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -75,6 +75,8 @@ echo "translated text"
 
 
 title=$(grep -F "標題" $dirsub/LATEST/Translated.ass | grep -F "Dialogue" | awk '!/bord0/' | sed $SEDOPTION_L 's/.*,,0,0,0,,//' | rev | cut -d '}' -f1 | rev | uniq | grep -v '櫻桃小丸子' | sed $SEDOPTION_L 's/ //g' | sed $SEDOPTION_L 's/|/ + /g')
+echo aaa
+echo echo ${title%; }
 # | cut -f1-2 -d"+")
 echo "title grabbed ($title)"
 
