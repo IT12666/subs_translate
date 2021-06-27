daf5f80553cd487dcc71553c2c5f73f8195483f6
IT12666 <Anson.12666@gmail.com>
2021-06-27 16:31:24 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -74,7 +74,7 @@ opencc -i $dirsub/LATEST/Translated.ass -o $dirsub/LATEST/Translated.ass
 echo "translated text"
 
 
-title=$(grep -F "標題" $dirsub/LATEST/Translated.ass | grep -F "Dialogue" | awk '!/bord0/' | sed $SEDOPTION_L 's/.*,,0,0,0,,//' | rev | cut -d '}' -f1 | rev | uniq | grep -v '櫻桃小丸子' | sed $SEDOPTION_L 's/ //g' | sed $SEDOPTION_L 's/|/ + /g' | head -n -2 | awk 'NF' | tr '\r' ',')
+title=$(grep -F "標題" $dirsub/LATEST/Translated.ass | grep -F "Dialogue" | awk '!/bord0/' | sed $SEDOPTION_L 's/.*,,0,0,0,,//' | rev | cut -d '}' -f1 | rev | uniq | grep -v '櫻桃小丸子' | sed $SEDOPTION_L 's/ //g' | sed $SEDOPTION_L 's/|/ + /g' | head -n -2 | awk 'NF' | tr '\r' ','| tr '\n' ',')
 
 #rm -f $dirsub/LATEST/tmp.txt
 echo "title grabbed ($title)"
