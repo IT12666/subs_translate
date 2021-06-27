1bdcd4feb4e594282baca29ad46483f07faaec92
IT12666 <Anson.12666@gmail.com>
2021-06-27 12:48:27 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -2,7 +2,7 @@
 
 #SYSTEM PRESET
 if [[ "$OSTYPE" == "darwin"* ]]; then SEDOPTION="-i ''"; else SEDOPTION="-ri"; fi
-if [[ "$OSTYPE" == "darwin"* ]]; then SEDOPTION_L=""; else SEDOPTION_L="-ri"; fi
+if [[ "$OSTYPE" == "darwin"* ]]; then SEDOPTION_L=""; else SEDOPTION_L=""; fi
 
 currdir=$(dirname "$0")
 setup=$currdir/_ESSENTIAL/Setup.txt
