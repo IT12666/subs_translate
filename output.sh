d25ffb8912f51de5d0cd77f452208525b25204bd
IT12666 <Anson.12666@gmail.com>
2021-06-28 01:15:39 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -77,8 +77,8 @@ opencc -i $dirsub/LATEST/Translated.ass -o $dirsub/LATEST/Translated.ass
 echo "translated text"
 
 
-title=$(grep -F "標題" $dirsub/LATEST/Translated.ass | grep -F "Dialogue" | awk '!/bord0/' | sed $SEDOPTION_L 's/.*,,0,0,0,,//' | rev | cut -d '}' -f1 | rev | uniq | grep -v '櫻桃小丸子' | sed $SEDOPTION_L 's/ //g' )
-#| head -n -2 | awk 'NF' | tr '\r' ',' | tr '\n' ',' | sed $SEDOPTION_L 's/,,/,/g' | sed $SEDOPTION_L 's/,/ + /g' | sed 's/.\{2\}$//'
+title=$(grep -F "標題" $dirsub/LATEST/Translated.ass | grep -F "Dialogue" | awk '!/bord0/' | sed $SEDOPTION_L 's/.*,,0,0,0,,//' | rev | cut -d '}' -f1 | rev | uniq | grep -v '櫻桃小丸子' | sed $SEDOPTION_L 's/ //g' | awk 'NF' | tr '\r' ',' | tr '\n' ',' )
+#| head -n -2 | sed $SEDOPTION_L 's/,,/,/g' | sed $SEDOPTION_L 's/,/ + /g' | sed 's/.\{2\}$//'
 echo "title grabbed ($title)"
 
 
