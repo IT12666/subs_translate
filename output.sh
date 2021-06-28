00e9f565ab139474a0ba70a31cddead547414fc3
IT12666 <Anson.12666@gmail.com>
2021-06-28 11:27:22 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -25,6 +25,7 @@ dep_check 'ffmpeg' 'brew install ffmpeg'
 if ! command -v $1 > /dev/null; then echo "could not install packages, please make sure you install brew correctly." && exit 0; fi
 echo "dependency checked"
 
+read -p "$(date +'%m-%d-%Y-' + '%r')"
 
 rm -f $currdir/_ESSENTIAL/log.txt
 for dirsub in $(echo $currdir/*/) ; do
