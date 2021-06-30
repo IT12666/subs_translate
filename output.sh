f6f9964796b38c215e12482de05dc5457621dbba
IT12666 <Anson.12666@gmail.com>
2021-07-01 00:47:01 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -63,8 +63,8 @@ if grep -q "$(grep -F $epname".keywords=" $setup | cut -d "=" -f2)" $dirsub/LATE
 while read line; do source=$(echo $line | rev | cut -d'|' -f 2 | rev) && result=$(echo $line | cut -d'|' -f 2) && sed $SEDOPTION "s!$source!$result!g"  $dirsub/LATEST/Translated.ass; done < $(dirname $dirsub)"/_ESSENTIAL/Replacement/"$(basename $dirsub)"/Font.txt"
 echo "translated font"
 
+if [ ! -f $dirsub/LATEST/Cover.jpg ] ; then echo "No Cover found, Downloading..." && curl -s -L -o $dirsub/LATEST/Cover.jpg $(grep -F $epname".cover=" $setup | cut -d "=" -f2) ; fi
 cover=$(curl -s --upload-file $dirsub/LATEST/Cover.jpg https://transfer.sh/yysub.jpg 2>/dev/null)
-: "${cover:= $(grep -F $epname".cover=" $setup | cut -d "=" -f2)}"
 echo "uploaded cover"
 
 filech=$(grep -F "Video File: " $dirsub/LATEST/Translated.ass | sed $SEDOPTION_L 's/Video File: //g')
