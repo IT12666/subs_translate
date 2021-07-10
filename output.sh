38a75427b1edf97490fff105d57857be4b18c775
IT12666 <Anson.12666@gmail.com>
2021-07-10 22:31:48 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -11,8 +11,8 @@ runupdate() { curl -s -L -o $currdir/update.sh https://raw.githubusercontent.com
 chmod +x $currdir/update.sh && exec $currdir/update.sh && echo "Updating" && exec $currdir/update.sh
 }
 
-#if [ ! -f $setup ]; then runupdate; fi
-#if [ "$(grep -F "Sys.updated=" $setup | cut -d "=" -f2)" == "1" ]; then echo "Latest Version"; else runupdate; fi 
+if [ ! -f $setup ]; then runupdate; fi
+if [ "$(grep -F "Sys.updated=" $setup | cut -d "=" -f2)" == "1" ]; then echo "Latest Version"; else runupdate; fi 
 
 dep_check() { if ! command -v $1 > /dev/null; then echo $1 is missing
 read -t 4 -n 1 -p "do you want to install $1? [Y]/N : " install
