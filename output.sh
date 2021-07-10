847e43941deb5bcc0f0719681974b4bfe8ddae6f
IT12666 <Anson.12666@gmail.com>
2021-07-10 21:35:49 +0800

    Update output.sh

--- output.sh
+++ output.sh
@@ -25,8 +25,7 @@ dep_check 'ffmpeg' 'brew install ffmpeg'
 #if [ ! command -v opencc > /dev/null ] || [ ! command -v ffmpeg > /dev/null ] ; then echo "could not install packages, please make sure you install brew correctly." && exit 0; fi
 echo "dependency checked"
 
-echo ls -t1 $currdir/_ESSENTIAL/log/
-ls -t1 $currdir/_ESSENTIAL/log/ | tail -n +22 | xargs rm -f
+for logfile in $(ls -t1 $currdir/_ESSENTIAL/log/ | head -15 ); do echo rm -f $currdir/_ESSENTIAL/log/$logfile; done
 echo "deleted old logs"
 
 
