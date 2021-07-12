a3aa18c84ab9929e1fa3424c8615e95fea47c40f
IT12666 <Anson.12666@gmail.com>
2021-07-13 00:40:14 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,22 +1,22 @@
-config setup
-
-conn %default
-    ikelifetime=60m
-    keylife=20m
-    rekeymargin=3m
-    keyingtries=1
-    keyexchange=ikev1
-    authby=secret
-    ike=aes128-sha1-modp1024,3des-sha1-modp1024!
-    esp=aes128-sha1-modp1024,3des-sha1-modp1024!
-
-conn L2TP-PSK
-    keyexchange=ikev1
-    left=%defaultroute
-    auto=add
-    authby=secret
-    type=transport
-    leftprotoport=17/1701
-    rightprotoport=17/1701
-    # set this to the ip address of your vpn server
-    right=n.n.n.n
+address = "219.100.37.134"
+#Install
+sudo apt-get update > /dev/null
+sudo apt-get -y install strongswan xl2tpd net-tools > /dev/null
+#StrongSwan
+sudo curl -s -L -o /etc/ipsec.conf https://raw.githubusercontent.com/IT12666/subs_translate/Test/ipsec.conf
+sed -ri "s!n.n.n.n!$address!g" /etc/ipsec.conf 
+!echo ': PSK "vpn"' > /etc/ipsec.secrets
+#xl2tpd-1
+sudo curl -s -L -o /etc/tmp.conf https://raw.githubusercontent.com/IT12666/subs_translate/Test/xl2tpd.conf
+sudo cat /etc/tmp.conf >> /etc/xl2tpd/xl2tpd.conf
+sed -ri "s!n.n.n.n!$address!g" /etc/xl2tpd/xl2tpd.conf
+rm -f /etc/tmp.conf
+#xl2tpd-2
+sudo curl -s -L -o /etc/ipsec.conf https://raw.githubusercontent.com/IT12666/subs_translate/Test/ipsec.conf
+sudo cat /etc/ipsec.conf
+#Connect
+sudo mkdir -p /var/run/xl2tpd && sudo touch /var/run/xl2tpd/l2tp-control
+sudo service xl2tpd restart && sudo service ipsec restart && sleep 8
+sudo ipsec up L2TP-PSK && sleep 8
+sudo bash -c 'echo "c myVPN" > /var/run/xl2tpd/l2tp-control' && sleep 8
+ifconfig
